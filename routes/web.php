2e0b557c6f4101ab74618e2f3fa7ae2f4ccdd58f
leeloxs <leeloxs@gmail.com>
2021-03-08 16:32:16 +0800

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes ( pages link )
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+
+//Route::get('/profile', function () {
+  //  return view('profile');
+//});
+
+Route::view("contact",'contact'); 
+Route::view("profile",'profile'); 
+//Route:get('/contact',[ContactController::class,'contact']);   //contact-form & contact function 
+//Route::post('/send-message',[ContactController::class,'sendEmail'])->name(contact.send);
